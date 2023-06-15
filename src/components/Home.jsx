8b822310d9a8f3b563e8c53b2b7e8f80d0e444b9
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-15 12:05:38 +0700

    add responsive in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -50,15 +50,15 @@ const Home = () => {
   }
 
   return (
-    <div className='mx-24 my-12 md:mx-32 lg:mx-40 xl:mx-72 2xl:mx-[30rem]'>
-      <div className='text-center text-4xl font-semibold mb-4'>Weather App</div>
+    <div className='mx-8 my-8 md:mx-32 lg:mx-40 xl:mx-72 2xl:mx-[30rem]'>
+      <div className='text-center text-3xl font-semibold mb-4'>Weather App</div>
       <div className='flex justify-center'>
-        <div className="relative mb-4 w-full lg:max-w-lg">
+        <div className="relative mb-4 w-full">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
             <input value={searchQuery} onChange={handleInputChange} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none" placeholder="Search city..." required />
-            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <button type="submit" className="absolute right-2.5 bottom-2.5 bg-blue-300 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-4 py-2 text-gray-900">Search</button>
         </div>
       </div>
 
@@ -67,9 +67,9 @@ const Home = () => {
       {
         searchQuery != '' && matchCities.length == 0
           ? <div className='text-center mt-10 font-semibold text-red-500 text-xl'>No data</div>
-          : <div className='flex flex-wrap items-center justify-center'>
+          : <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
               {matchCities.map((city) => (
-                <div  key={city.id} className='w-full md:w-1/3 lg:w-1/4 mb-2 mx-4 md:mb-4'>
+                <div key={city.id} className='w-full'>
                   <Link to={`/${city.slug}?lat=${city.latitude}&long=${city.longitude}&region=${city.name}`}>
                     <div className='rounded-md p-4 bg-blue-100 hover:bg-blue-200 hover:cursor-pointer'>
                       <p className='text-center'>{city.name}</p>
@@ -81,12 +81,12 @@ const Home = () => {
       }
 
       {/* pagination nav */}
-      <div className='flex justify-between items-center mx-12'>
-        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className='disabled:bg-opacity-50 bg-slate-300 px-4 py-2 rounded-lg disabled:cursor-not-allowed'>
-          Previous
+      <div className='flex justify-between items-center mt-4 lg:mt-8 2xl:mt-10 lg:mx-24'>
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className='disabled:bg-opacity-50 bg-slate-300 px-4 py-2 md:px-6 lg:px-8 rounded-lg disabled:cursor-not-allowed'>
+          &lt; Previous
         </button>
-        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className='disabled:bg-opacity-50 bg-slate-300 px-4 py-2 rounded-lg disabled:cursor-not-allowed'>
-          Next
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className='disabled:bg-opacity-50 bg-slate-300 px-4 py-2 md:px-6 lg:px-8 rounded-lg disabled:cursor-not-allowed'>
+          Next &gt;
         </button>
       </div>
     </div>
